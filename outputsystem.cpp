84b5f9e9841f5ce4d344f45f5310e7b1081b3284
deathstroke44 <saminyeaserlight@gmail.com>
2018-06-23 15:27:28 +0600

    algo
    algo

---
+++ outputsystem.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ofstream myfile;
+void solve()
+{
+}
+int main()
+{
+	
+	myfile.open ("example.txt");
+	int t;cin>>t;for(int i=0;i<t;i++) solve();
+	
+	myfile.close();
+	return 0; 
+}
